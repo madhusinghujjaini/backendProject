ba68cfe86b96d5e2be1e01f671b20122fa099a72
madhusinghujjaini <madhu08aug@gmail.com>
2022-09-09 10:37:01 +0530

    added db

--- app.js
+++ app.js
@@ -1,8 +1,23 @@
 const express = require('express');
-const bodyParser = require('body-parser')
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+
+const Task = require('./model/Task')
 
 const app = express();
 const PORT = 3000;
+
+
+const connectToDatabase = async ()=>{
+    try {
+        await mongoose.connect('mongodb+srv://admin:admin@todoapp.ysdlh3a.mongodb.net/?retryWrites=true&w=majority');
+        console.log("Connected to db")
+    } catch (error) {
+        console.log(error);
+    }
+    
+}
+connectToDatabase();
 
 app.use(bodyParser.json())
 app.listen(PORT, () => {
@@ -11,24 +26,38 @@ app.listen(PORT, () => {
 
 let taskList = [];
 
-app.get('/getTaskList', (req, res) => {
-    res.status(200).send(taskList)
+app.get('/getTaskList', async (req, res) => {
+    const tasks = await Task.find({})
+    res.status(200).send(tasks)
 })
 
-app.post('/addTask', (req, res) => {
-    let taskId = Math.random();
-    req.body.id = getRendomID();
-    taskList.push(req.body)
-    res.status(200).send({ sucess: 'date save sucessfully' })
-})
-
-app.put('/updateTask', (req, res) => {
-    console.log(req.body)
-    let task = taskList.find(ele=> {
-        return  ele.id == req.body.id
+app.post('/addTask', async (req, res) => {
+    // let taskId = Math.random();
+    // req.body.id = getRendomID();
+    // taskList.push(req.body)
+   
+    const result = await Task.create({
+        name: req.body.taskName,
+        description: req.body.description
     })
-    task.taskName = req.body.taskName;
+      
+    res.status(200).send({ sucess: `Task ${req.body.taskName} created sucessfully` });
+})
+
+app.put('/updateTask', async (req, res) => {
+    console.log(req.body)
+    // let task = taskList.find(ele=> {
+    //     return  ele.id == req.body.id
+    // })
+    // task.taskName = req.body.taskName;
+    // task.description = req.body.description;
+    
+    const task = await Task.findById(req.body.id);
+    task.name = req.body.taskName;
     task.description = req.body.description;
+
+    await task.save();
+
     res.status(200).send({ sucess: 'date updated sucessfully' })
 })
 
